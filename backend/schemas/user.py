bb290dc4ab0869c01c16aa9605b110f72d27a8b6
AlxKuzn <124476116+Iceinviz@users.noreply.github.com>
2024-11-30 09:13:33 +0400

    Add files via upload

---
+++ backend/schemas/user.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, EmailStr, Field
+
+class UserCreate(BaseModel):
+    login: str = Field(..., min_length=4)
+    password: str = Field(..., min_length=4)
+    email: EmailStr
+
+class ShowUser(BaseModel):
+    id: int
+    email: EmailStr
+
+    class Config():
+        orm_mode = True
